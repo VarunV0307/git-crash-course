4d3814405732a3f4291e9c459ad5277c6136a979
VarunV0307 <varunvinjamuri31@gmail.com>
2026-08-01 20:01:59 +0530

    Added new feature

--- index.py
+++ index.py
@@ -4,3 +4,12 @@ sum = a + b
 print("The sum of", a, "and", b, "is:", sum)
 print("The product of", a, "and", b, "is:", a * b)
 print("The difference of", a, "and", b, "is:", a - b)
+
+if a > b:
+    print(a, "is greater than", b)
+else:
+    print(b, "is greater than", a)
+if a == b:
+    print(a, "is equal to", b)
+else:
+    print(a, "is not equal to", b)
