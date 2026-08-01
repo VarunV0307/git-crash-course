8a34c09a6e861b9dc10b03ef82fd53a1cac317b0
VarunV0307 <varunvinjamuri31@gmail.com>
2026-08-01 17:43:36 +0530

    ABC commit

--- index.py
+++ index.py
@@ -2,3 +2,5 @@ a = int(input("Enter a number: "))
 b = int(input("Enter another number: "))
 sum = a + b 
 print("The sum of", a, "and", b, "is:", sum)
+print("The product of", a, "and", b, "is:", a * b)
+print("The difference of", a, "and", b, "is:", a - b)
