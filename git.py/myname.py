512f3682da2d031896fcdb1a5611d1da186e9b30
VarunV0307 <varunvinjamuri31@gmail.com>
2026-08-01 18:16:35 +0530

    Revert "addtion is preformed"
    This reverts commit aa7692aa0517b2418e1ea3a5753f6aab36714a7d.

--- git.py/myname.py
+++ git.py/myname.py
@@ -1,8 +1,4 @@
 a = 5
 b = 10
-c = 15
-d = 20
-print("The values are:", a, b, c, d)
-print("The sum of a and b is:", a + b)
-print("The sum of c and d is:", c + d)
-
+for i in range(a, b):
+    print(i)
