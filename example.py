5fd3f78c8a549eeb26461a9f2a4c6d7fb395d99b
VarunV0307 <varunvinjamuri31@gmail.com>
2026-08-01 17:45:27 +0530

    changes in example

--- example.py
+++ example.py
@@ -1,3 +1,4 @@
 print("Hello World")
 print ("my name is John")
 print("I am learning Python programming.")
+print("Python is a versatile language used for web development, data analysis, artificial intelligence, and more.")
