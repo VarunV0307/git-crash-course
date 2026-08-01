aa7692aa0517b2418e1ea3a5753f6aab36714a7d
VarunV0307 <varunvinjamuri31@gmail.com>
2026-08-01 17:18:30 +0530

    addtion is preformed

--- git.py/myname.py
+++ git.py/myname.py
@@ -1,4 +1,8 @@
 a = 5
 b = 10
-for i in range(a, b):
-    print(i)
+c = 15
+d = 20
+print("The values are:", a, b, c, d)
+print("The sum of a and b is:", a + b)
+print("The sum of c and d is:", c + d)
+
