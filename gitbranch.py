8dbf3677e5adf15e46acc47b15e236f03d2a11a6
VarunV0307 <varunvinjamuri31@gmail.com>
2026-08-09 07:07:15 +0530

    new commit1

---
+++ gitbranch.py
@@ -0,0 +1,13 @@
+num = 5
+
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
+
+num1 = 10
+num2 = 20
+if num1 > num2:
+    print(f"{num1} is greater than {num2}")
+else:
+    print(f"{num2} is greater than {num1}")
+    if num1 == num2:
+        print(f"{num1} is equal to {num2}")
